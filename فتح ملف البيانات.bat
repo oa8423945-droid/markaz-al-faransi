377c2c35d0786c64cdd1ced8aaeea24713b568d2
oa8423945-droid <oa8423945@gmail.com>
2026-08-25 19:06:35 +0300

    Fix Windows installer file staging

--- فتح ملف البيانات.bat
+++ فتح ملف البيانات.bat
@@ -1,9 +1,3 @@
 @echo off
-cd /d "%~dp0"
-if not exist "data\main data 2.xlsx" goto missing
-start "" "%CD%\data\main data 2.xlsx"
-exit /b 0
-:missing
-echo data\main data 2.xlsx was not found.
-pause
-exit /b 1
+call "%~dp0open-data.bat"
+
