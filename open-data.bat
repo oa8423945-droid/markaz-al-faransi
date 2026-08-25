fb8bbab87eb4e2bf5459cd5fac50e77a1ceb1eda
oa8423945-droid <oa8423945@gmail.com>
2026-08-25 19:06:25 +0300

    Fix Windows installer file staging

---
+++ open-data.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d "%~dp0"
+if not exist "data\main data 2.xlsx" goto missing
+start "" "%CD%\data\main data 2.xlsx"
+exit /b 0
+
+:missing
+echo The database file data\main data 2.xlsx was not found.
+pause
+exit /b 1
+
